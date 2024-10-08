66e587738c44928d20cadbae60215a2a67f66e3b
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2024-10-09 01:47:47 +0700

    make the login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,33 @@
 import { FC } from "react";
+import styled from "styled-components";
+import image from "assets/images/intro-animation.gif";
+import Login from "components/Login";
+
+const Root = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ImgBox = styled.div`
+  width: 40%;
+  height: 40%;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-image: url(${image});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain, cover;
+`;
 
 const LoginPage: FC = () => {
-  return <>adasdas</>;
+  return (
+    <Root>
+      <ImgBox />
+      <Login />
+    </Root>
+  );
 };
 
 export default LoginPage;
