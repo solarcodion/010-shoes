20b943288112e6cf70dfffd7237b92bdad4fb356
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2024-10-14 21:33:56 +0700

    detect the useless url

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,6 +1,16 @@
 import Welcome from "components/Welcome";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const WelcomePage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (sessionStorage.getItem("user") === "1") {
+      navigate(-1);
+    }
+  });
+
   return <Welcome />;
 };
 
