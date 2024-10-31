e328bd9cbc66535acd0089e2fb8bd08473943d01
phoenix1004-dev <181405513+phoenix1004-dev@users.noreply.github.com>
2024-10-31 22:08:37 +0700

    add initial chip page

---
+++ src/pages/ChipPage.tsx
@@ -0,0 +1,12 @@
+import Chip from "containers/Chip";
+import WithAppbar from "layouts/WithAppbar";
+
+const ChipPage = () => {
+  return (
+    <WithAppbar>
+      <Chip />
+    </WithAppbar>
+  );
+};
+
+export default ChipPage;
