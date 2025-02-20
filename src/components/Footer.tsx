7cf5073840315546ef4ffda03b4e259bd84c559f
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-02-21 05:36:35 +0700

    add icons and change name for new social link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,13 +12,21 @@ const Root = styled.div`
 `;
 
 const Link = styled.a`
+  display: flex;
   font-size: 14px;
   color: ${(props) => props.theme.colors.text};
   opacity: 0.5;
+  gap: 3px;
   text-decoration: none;
   &:hover {
     opacity: 1;
   }
+`;
+
+const SocialIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  color: white;
 `;
 
 const Footer = () => {
@@ -32,6 +40,7 @@ const Footer = () => {
         {SOCIAL_LINKS.map((link) => {
           return (
             <Link href={link.url} key={link.url} target="_blank">
+              <SocialIcon src={link.icon} alt={link.label} />
               {link.label}
             </Link>
           );
