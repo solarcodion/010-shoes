30ad77e7bae98b5487a360c6afde802686b66dfe
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-02-21 23:25:23 +0700

    detect loop

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,7 +3,7 @@ export const getNextLink = (links: string[], current: string) => {
   if (index > -1) {
     index++;
     if (index > links.length - 1) {
-      index = 0;
+      index = links.length - 1;
     }
     return links[index];
   }
@@ -15,7 +15,7 @@ export const getPrevLink = (links: string[], current: string) => {
   if (index > -1) {
     index--;
     if (index < 0) {
-      index = links.length - 1;
+      index = 0;
     }
     return links[index];
   }
