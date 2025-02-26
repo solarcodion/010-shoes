d7483c8b25f5e61aab899009d14ad625d691dc79
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-02-26 19:00:07 +0700

    fix the logo background in login page

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import image from "assets/images/sneaker-intro-1.png";
+import logo from "assets/images/logo/Logo 010.png";
 import Login from "components/Login";
+import { device } from "utils/device";
 
 const Root = styled.div`
   width: 100vw;
@@ -20,11 +22,36 @@ const ImgBox = styled.div`
   background-repeat: no-repeat;
   background-position: center;
   background-size: contain, cover;
+
+  @media ${device.mobile} {
+    max-width: 600px;
+  }
+`;
+
+const LogoBox = styled.div`
+  width: 60%;
+  height: 70%;
+  max-height: 700px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  aspect-ratio: 2/1;
+  transform: translate(-50%, -50%);
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain, cover;
+
+  @media ${device.mobile} {
+    width: 100%;
+    max-width: 600px;
+  }
 `;
 
 const LoginContainer = () => {
   return (
     <Root>
+      <LogoBox />
       <ImgBox />
       <Login />
     </Root>
