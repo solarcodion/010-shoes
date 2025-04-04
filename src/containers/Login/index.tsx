cea41a555c78107f2a1dd605049c4e1919d94ed4
solarcodion <181405513+solarcodion@users.noreply.github.com>
2025-04-04 18:15:58 +0700

    add glowing animation

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import image from "assets/images/sneaker-intro-1.png";
-import logo from "assets/images/logo/Logo 010.png";
+import xPopup from "assets/images/logo/x-popup.svg";
+import oPopup from "assets/images/logo/o-popup.svg";
 import Login from "components/Login";
 import { device } from "utils/device";
+import { useEffect, useState } from "react";
 
 const Root = styled.div`
   width: 100vw;
@@ -29,17 +31,38 @@ const ImgBox = styled.div`
   }
 `;
 
-const LogoBox = styled.div`
+const XLogoBox = styled.div`
+  width: 50%;
+  height: 50%;
+  max-width: 600px;
+  max-height: 600px;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  aspect-ratio: 1/1;
+  transform: translate(-50%, -50%);
+  background-image: url(${xPopup});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain, cover;
+
+  @media ${device.mobile} {
+    width: 100%;
+    max-width: 600px;
+  }
+`;
+
+const OLogoBox = styled.div`
   width: 60%;
-  height: 70%;
-  max-width: 900px;
+  height: 60%;
+  max-width: 700px;
   max-height: 700px;
   position: fixed;
   top: 50%;
   left: 50%;
-  aspect-ratio: 2/1;
+  aspect-ratio: 1/1;
   transform: translate(-50%, -50%);
-  background-image: url(${logo});
+  background-image: url(${oPopup});
   background-repeat: no-repeat;
   background-position: center;
   background-size: contain, cover;
@@ -51,9 +74,22 @@ const LogoBox = styled.div`
 `;
 
 const LoginContainer = () => {
+  const [flag, setFlag] = useState<boolean>(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      toggleFlag();
+    }, 1000);
+  });
+
+  const toggleFlag = () => {
+    setFlag((prev) => !prev);
+  };
+
   return (
     <Root>
-      <LogoBox />
+      {flag && <XLogoBox />}
+      {!flag && <OLogoBox />}
       <ImgBox />
       <Login />
     </Root>
